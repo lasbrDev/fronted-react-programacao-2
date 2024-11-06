56a7cbbef2f952620d2b687262123377a2ef890c
Luciano Alves <lasbr_alves@outlook.com>
2024-11-06 15:57:57 -0300

    feat: added Telas/TelaCadastroCliente, Telas/TelaCadastroFornecedor

---
+++ src/Telas/TelaCadastroCliente.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import Pagina from "../templates/Pagina";
+import FormCadCliente from "./Formularios/FormCadCliente";
+
+export default function TelaCadastroCliente(props) {
+    return (
+        <Pagina>
+            <Container className="mt-2">
+                <h2 className="text-center">Tela Cadastro de Clientes</h2>
+                <FormCadCliente />
+            </Container>
+        </Pagina>
+    );   
+}   
---
+++ src/Telas/TelaCadastroFornecedor.jsx
@@ -0,0 +1,10 @@
+import Pagina from "../templates/Pagina";
+
+export default function TelaCadastroFornecedor(props) {
+    return (
+        <Pagina>
+            <h1>Tela Cadastro de Fornecedores</h1>
+            <h2>Formulário</h2>
+        </Pagina>
+    );
+}
